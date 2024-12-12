4b2f104b52e4a99208d038039336c85d0cb5d9bd
Ardeleanu-Vlad-George <ardeleanu_vlad@ymail.com>
2024-12-12 14:05:28 +0200

    Added function declaration + updated documentation

--- inc/path.h
+++ inc/path.h
@@ -11,9 +11,13 @@
     - 'base': the path up to the 'file' part;
     - 'name': the name of the file without the extension;
     - 'extn': the extension of the file;
-  There are two functions, their purpose well stated by the their name 
+  There are two functions that offer certain components from the 
+  above list expecting the input to also be one of those components
+  The third function returns an array of C style strings, each
+  position points to, in the following order, the folders up to the 
+  'file' part, the 'name' component and the 'extn' component.
   The functions will modify the original string, the user is expected to
-  in mind.
+  have that in mind.
 */
 
 /**
@@ -31,5 +35,21 @@ void path_to_base_and_file(char** path_to_base, char** file);
   @param extn         [out]   The extension of the file
 */
 void file_to_name_and_extn(char** file_to_name, char** extn);
+
+
+/**
+  \brief Convert a full 'path' into tokens 
+  The first tokens are the folder making up the 'base' part 
+  if they exist, the next one is 'name', the last one is 'extn'
+  This parts are given only if they exist
+  The number of found tokens is saved in the second argument
+  The array of found tokens is returned by the function
+
+  More details down below: 
+  @param path [inout] C style string to be processed
+  @param size [out]   The number of found tokens
+  \return tokns An array of C style strings containing the tokens 
+*/
+char** path_to_tokn(char* path, int *size);
 
 #endif//_PATH_H_
